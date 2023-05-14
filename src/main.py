898de41d08b16932e011f1c5648fde61babd390b
Georgios Giasemidis <g.giasemidis@gmail.com>
2023-05-15 00:03:01 +0100

    formatting changes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import os
 import openai
 import streamlit as st
 from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
 
 openai.organization = os.getenv("OPENAI_API_ORG")
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -12,7 +11,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def get_text():
     """Input text by the user"""
     input_text = st.text_input(
-        "Ask me your question. Answers will be limited to 256 tokens", "", key="input"
+        "Ask me your question. Answers will be limited to 256 tokens",
+        "", key="input"
     )
     return input_text
 
@@ -20,20 +20,26 @@ def get_text():
 def generate_response_chat(messages):
     """Generate Reponse using GPT3.5 API"""
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0301", messages=messages, max_tokens=256, temperature=0
+        model="gpt-3.5-turbo-0301", messages=messages,
+        max_tokens=256, temperature=0
     )
     return response["choices"][0]["message"]
 
 
 def chatbot():
-    """Main chatbox function based on ChatCompletion API and GPT-3.5-turbo model"""
+    """
+    Main chatbox function based on ChatCompletion API
+    and GPT-3.5-turbo model
+    """
     st.title("MARTHA: Your marketing and advertising expert chatbox.")
 
     greeting_bot_msg = (
-        "Hi, I am MARTHA, your marketing & advertising expert. Ask me any related question.\n"
-        "Ah! I have no knowledge of 2022 onwards, because I am powered by ChatGPT. "
-        "So, I don't do predictions.\n"
-        "*Example*: 'What are the implications of the death of third party cookies for the industry?'\n"
+        "Hi, I am MARTHA, your marketing & advertising expert. "
+        "Ask me any related question.\n"
+        "Ah! I have no knowledge of 2022 onwards, because I am powered by "
+        "ChatGPT. So, I don't do predictions.\n"
+        "*Example*: 'What are the implications of the death of third party "
+        "cookies for the industry?'\n"
         "I don't answer questions like 'Who was US president in 2010?'"
     )
 
@@ -45,9 +51,10 @@ def chatbot():
         st.session_state["past"] = []
 
     prompt = (
-        "Classify if the following prompt questions are related to marketing and advertising. "
-        "If they are, answer the question. If they are not, reply only "
-        "'This is not a marketing question' and do not answer the question. "
+        "Classify if the following prompt questions are related to marketing "
+        "and advertising. If they are, answer the question. If they are not, "
+        "reply only 'This is not a marketing question' and do not answer the "
+        "question. "
         "Limit your answer to 50 tokens if possible."
     )
     if "messages" not in st.session_state:
@@ -58,7 +65,9 @@ def chatbot():
     user_input = get_text()
 
     if user_input:
-        st.session_state["messages"].append({"role": "user", "content": user_input})
+        st.session_state["messages"].append(
+            {"role": "user", "content": user_input}
+        )
         response = generate_response_chat(st.session_state["messages"])
         st.session_state.past.append(user_input)
         st.session_state.generated.append(response["content"])
@@ -69,7 +78,9 @@ def chatbot():
             message(st.session_state["generated"][i], key=str(i))
             if i - 1 >= 0:
                 message(
-                    st.session_state["past"][i - 1], is_user=True, key=str(i) + "_user"
+                    st.session_state["past"][i - 1],
+                    is_user=True,
+                    key=str(i) + "_user"
                 )
 
 
